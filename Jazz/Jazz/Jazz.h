66c8ada20e7df5ac0ef4b23739ab7f0932c621f6
sharjeelahmed <sharjeelahmed92@hotmail.com>
2020-06-10 23:19:52 +0500

    Project Initialize

---
+++ Jazz/Jazz/Jazz.h
@@ -0,0 +1,19 @@
+//
+//  Jazz.h
+//  Jazz
+//
+//  Created by Shairjeel Ahmed on 10/06/2020.
+//  Copyright © 2020 Shairjeel Ahmed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jazz.
+FOUNDATION_EXPORT double JazzVersionNumber;
+
+//! Project version string for Jazz.
+FOUNDATION_EXPORT const unsigned char JazzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jazz/PublicHeader.h>
+
+
